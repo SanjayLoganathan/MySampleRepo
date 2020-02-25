e4beaa75d246a041a7a61324c01f0a02a137947c
karthikeyan_P@contus <karthikeyan.p@contus.in>
2020-02-25 11:13:25 +0530

    New Label is added to the new Uilabel Branch.

--- MySampleProject/MySampleProject/ViewController.swift
+++ MySampleProject/MySampleProject/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("Hello World")
+        UILABELADDED()
+    }
+    
+    func UILABELADDED()
+    {
+        print("New Label is Added")
     }
 
 
